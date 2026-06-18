9d6d0b1db28abbe6084c1f40221ce9178e37ecc1
markson aigbodi <maigbodi@gmail.com>
2026-06-18 08:50:00 +0100

    removed addition

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,7 +2,7 @@ package com.demo;
 
 public class App {
     public static String greet() {
-        return "Hello CI Pipeline! with more code";
+        return "Hello CI Pipeline!";
     }
 
     public static void main(String[] args) {
